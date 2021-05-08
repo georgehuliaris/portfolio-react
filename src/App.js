f1e0c2ceefecc0fa1b746e071eb5a802041f309b
George Huliaris <georgehuliaris@me.com>
2021-05-08 15:46:51 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Profile
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
