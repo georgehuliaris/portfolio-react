1df8e9d571d8e074d65a43bf74ed6ed856be8954
George Huliaris <georgehuliaris@me.com>
2021-05-10 00:21:15 -0500

    done with styling contacts page

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,46 +1,87 @@
-import React from 'react';
+import React from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
-import { TextField, Typography, Button, Grid, Box} from "@material-ui/core";
+import { TextField, Typography, Button, Grid, Box } from "@material-ui/core";
 import SendIcon from "@material-ui/icons/Send";
 import Navbar from "./Navbar";
 
+const useStyles = makeStyles((theme) => ({
+  form: {
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    position: "absolute",
+  },
+  button: {
+    marginTop: "1rem",
+    color: "tomato",
+    borderColor: "tomato",
+  },
+}));
+
 const InputField = withStyles({
-    root: {
-        "& label.Mui-focused": {
-            color: "tomato",
-        },
-        "& label": {
-            color: "tan",
-        },
-        "& .MuiOutlinedInput-root": {
-            "& fieldset": {
-                borderColor: "tan",
-            },
-            "&:hover fieldset": {
-                borderColor: "tomato",
-            },
-            "&.Mui-focused fieldset": {
-                borderColor: "tan",
-            }
-        },
+  root: {
+    "& label.Mui-focused": {
+      color: "tomato",
     },
+    "& label": {
+      color: "tan",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "tan",
+      },
+      "&:hover fieldset": {
+        borderColor: "tomato",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "tan",
+      },
+    },
+  },
 })(TextField);
 
-
-
 const Contacts = () => {
-    return (
-        <Box component="div">
-            <Grid container justify="center">
-                <Box component="form">
-                    <Typography variant="h5">
-                    Hire or contact me!
-                    </Typography>
-                     <InputField fullWidth={true} label="Name" variant="outlined" />
-                </Box>
-            </Grid>
+  const classes = useStyles();
+  return (
+    <Box component="div" style={{ background: "#233", height: "100vh" }}>
+        <Navbar />
+      <Grid container justify="center">
+        <Box component="form" className={classes.form}>
+          <Typography variant="h5" style={{color: "tomato", textAlign: "center", textTransform: "uppercase" }}>Hire or contact me!</Typography>
+          <InputField
+            fullWidth={true}
+            label="Name"
+            variant="outlined"
+            inputProps={{ style: { color: "white" } }}
+            margin="dense"
+            size="medium"
+          />
+          <br />
+          <InputField
+            fullWidth={true}
+            label="Email"
+            variant="outlined"
+            inputProps={{ style: { color: "white" } }}
+            margin="dense"
+            size="medium"
+          />
+          <br />
+          <InputField
+            fullWidth={true}
+            label="Company"
+            variant="outlined"
+            inputProps={{ style: { color: "white" } }}
+            margin="dense"
+            size="medium"
+          />
+          <br />
+          <Button className={classes.button} variant="outlined" fullWidth={true} endIcon={<SendIcon />}>
+            contact me
+          </Button>
         </Box>
-    )
-}
+      </Grid>
+    </Box>
+  );
+};
 
 export default Contacts;
